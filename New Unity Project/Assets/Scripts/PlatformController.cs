68319db29cdde8bebe4bd7fabf53f2dba17d4226
Patrycja Cader <patcad@o2.pl>
2020-07-07 22:58:56 +0200

    project

---
+++ New Unity Project/Assets/Scripts/PlatformController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : MonoBehaviour
+{
+    // destinations, targets
+    public Transform[] targets;
+
+    // speed
+    public float speed = 1;
+
+    // flag that sets weather we are moving or not
+    bool isMoving = false;
+
+    // next destination index
+    int nextIndex;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // set the player to the first target
+        transform.position = targets[0].position;
+
+        // next destination is 1;
+        nextIndex = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // check for input
+        HandleInput();
+
+        // move the platform
+        HandleMovement();
+    }
+
+    void HandleInput()
+    {
+        // check for Fire1 axis
+        if (Input.GetButtonDown("Fire1"))
+        {
+            // negate isMoving
+            isMoving = !isMoving;
+        }
+    }
+
+    // take care of movement
+    void HandleMovement()
+    {
+        // if we are not moving exit
+        if (!isMoving) return;
+
+        //calculate the distance form target
+        float distance = Vector3.Distance(transform.position, targets[nextIndex].position);
+
+        //have we arrived?
+        if (distance > 0)
+        {
+
+            // calcuate how much we need to move (step) d = v * t
+            float step = speed * Time.deltaTime;
+
+            //move by that step
+            transform.position = Vector3.MoveTowards(transform.position, targets[nextIndex].position, step);
+        }
+        // if we have arrived we should update nextIndex
+        else
+        {
+            // next index increased by one
+            nextIndex++;
+
+            // array element index starst at 0 and goes all the wat to length-1
+            if (nextIndex == targets.Length)
+            {
+                nextIndex = 0;
+            }
+
+            // stop moving 
+            isMoving = false;
+        }
+    }
+}
---
+++ New Unity Project/Assets/ZenvaVR/Scripts/DragCamera.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZenvaVR
+{
+    public class DragCamera : MonoBehaviour
+    {
+#if UNITY_EDITOR
+        // flag to keep track wheather we are draging or not 
+        bool isDragging = false;
+
+        // starting point of a camera movement 
+        float startMouseX;
+        float startMouseY;
+
+        // camera component 
+        Camera cam;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            // get our camera component
+            cam = GetComponent<Camera>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // if we press the left button and we haven't started dragging 
+            if (Input.GetMouseButtonDown(1) && !isDragging)
+            {
+                // set the flag to true
+                isDragging = true;
+
+                // save the mouse to starting position 
+                startMouseX = Input.mousePosition.x;
+                startMouseY = Input.mousePosition.y;
+
+            }
+            // if we are not pressing the left button, and we have started dragging 
+            else if (Input.GetMouseButtonUp(1) && isDragging)
+            {
+                // set the flag to false 
+                isDragging = false;
+            }
+        }
+
+        void LateUpdate()
+        {
+            // check if we are dragging 
+            if (isDragging)
+            {
+                // Calculate current mouse position
+                float endMouseX = Input.mousePosition.x;
+                float endMouseY = Input.mousePosition.y;
+
+                // Difference (in screen coordinates)
+                float diffX = endMouseX - startMouseX;
+                float diffY = endMouseY - startMouseY;
+
+                // New center of a screen
+                float newCenterX = Screen.width / 2 + diffX;
+                float newCenterY = Screen.height / 2 + diffY;
+
+                // get the world coordinate, this is where we should be looking at
+                Vector3 LookHerePoint = cam.ScreenToWorldPoint(new Vector3(newCenterX, newCenterY, cam.nearClipPlane));
+
+                // make our camera look at the 'LookHerePoint'
+                transform.LookAt(LookHerePoint);
+
+                // starting position for the next call 
+                startMouseX = endMouseX;
+                startMouseY = endMouseY;
+            }
+        }
+#endif
+    }
+}
